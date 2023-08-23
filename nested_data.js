433e4b3335627b3e4e0a4cae64dd58f603faa868
Chase Van Halen <cvanhalen@perscholas.org>
2023-08-23 10:14:38 -0700

    first commit

---
+++ nested_data.js
@@ -0,0 +1,53 @@
+const zoo = {
+  name: "Wonderland Zoo",
+  location: "Safariville",
+  exhibits: {
+    mammals: {
+      animals: ["Lions", "Elephants", "Giraffes"],
+      curator: "Zoologist Jane"
+    },
+    reptiles: {
+      animals: ["Snakes", "Crocodiles", "Turtles"],
+      curator: "Herpetologist Mike"
+    }
+  },
+  visitors: [
+    {
+      name: "Emily",
+      age: 25,
+      ticketType: "Adult"
+    },
+    {
+      name: "Sam",
+      age: 8,
+      ticketType: "Child"
+    }
+  ]
+};
+
+const zoo2 = {
+  name: "Wildlife Sanctuary Park",
+  location: "Natureville",
+  habitats: {
+    birds: {
+      species: ["Eagles", "Parrots", "Owls"],
+      keeper: "Ornithologist Alex"
+    },
+    aquatic: {
+      species: ["Dolphins", "Turtles", "Penguins"],
+      keeper: "Marine Biologist Mia"
+    }
+  },
+  visitors: [
+    {
+      name: "Daniel",
+      age: 32,
+      ticketType: "Adult"
+    },
+    {
+      name: "Sophie",
+      age: 10,
+      ticketType: "Child"
+    }
+  ]
+};
